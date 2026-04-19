ebe8be27b754ead45c14b2d0cdac4b9b0621b545
sanaalothman99 <sanaalothmann@gmail.com>
2026-04-19 12:25:32 +0200

    FIX  :add app.listen

--- src/main.ts
+++ src/main.ts
@@ -5,8 +5,16 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     rawBody: true,
   });
-app.enableCors({
-  origin: ['https://course-platform-gkdsevvbd-sanaalothman99s-projects.vercel.app', 'http://localhost:3000'],
-  credentials: true,
-});}
+  
+  app.enableCors({
+    origin: ['https://course-platform-gkdsevvbd-sanaalothman99s-projects.vercel.app', 'http://localhost:3000'],
+    credentials: true,
+  });
+
+  const port = process.env.PORT || 3001;
+  await app.listen(port);
+  
+  console.log(`Application running on port ${port}`);
+}
+
 bootstrap();
