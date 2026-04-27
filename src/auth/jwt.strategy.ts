877d9dc0553fcab6692f90bb9aa4f1f1dd211f58
sanaalothman99 <sanaalothmann@gmail.com>
2026-04-27 12:29:48 +0200

    fix jwt secert in strategy

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -8,7 +8,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'your-secret-key-change-this',
+      secretOrKey: process.env.JWT_SECRET || 'your-secret-key-change-this',
     });
   }
 
